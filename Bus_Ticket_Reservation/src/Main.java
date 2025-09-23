b9cb945b873a42c655c751da9e452829408f4b79
RahulR015 <rahulr15ec@gmail.com>
2025-09-23 04:28:10 -0700

    commit succesfull

--- Bus_Ticket_Reservation/src/Main.java
+++ Bus_Ticket_Reservation/src/Main.java
@@ -3,8 +3,8 @@ class Main
 {
     public static void main(String[] args)
     {
-        ArrayList<Bus> buses=new ArrayList<Bus>();
-        ArrayList<Booking> bookings = new ArrayList<Booking>();
+        ArrayList<Bus> buses=new ArrayList<>();
+        ArrayList<Booking> bookings = new ArrayList<>();
         buses.add(new Bus(1,true,2));
         buses.add(new Bus(2,false,1));
         buses.add(new Bus(3,true,3));
